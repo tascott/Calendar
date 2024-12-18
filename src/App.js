05a89c8f1f69f71e553b8b1fa2f34145c5429c96
TomScott <contact@tascott.co.uk>
2024-12-18 22:37:02 +0000

    Starting again without a calendar library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import MyCalendar from './components/Calendar';
 
 function App() {
     return (
-        <div className="h-screen p-4 bg-gray-100">
-            <h1 className="text-3xl font-bold mb-4 text-center">
-                My Calendar App
-            </h1>
-            <MyCalendar />
+        <div className="w-full min-h-screen p-4 bg-gray-100">
+            Calendar
         </div>
     );
 }
